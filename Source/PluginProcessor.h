89698fac91f790c85d78c248c4219ed8eba293c9
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-01-15 12:49:08 +0100

    Added HP and LP filters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -166,6 +166,13 @@ public:
 private:
     
     CompressorBand compressor;
+    
+    using Filter = juce::dsp::LinkwitzRileyFilter<float>;
+    Filter LP, HP;
+    
+    juce::AudioParameterFloat* lowCrossover { nullptr };
+    
+    std::array<juce::AudioBuffer<float>, 2> filterBuffers;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
 };
